0f0c866c0ec0cf2e70f0e76807e75667e00c4213
Rahul Dubey <developerrahuldubey@gmail.com>
2021-03-08 17:22:36 -0800

    Added Project Skeleton

---
+++ EarthQuake/EarthQuake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarthQuake
+//
+//  Created by Rahul Dubey on 3/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
